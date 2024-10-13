299463f0ae291c810acf584afce1f00dd0cd483a
kayne-hadi <krhadipoespito@gmail.com>
2024-10-13 17:06:05 +0700

    Update home-script.js

--- home-script.js
+++ home-script.js
@@ -14,16 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
 
-    // Handle navigation to different pages
-    document.getElementById('arithmetic-btn').addEventListener('click', () => {
-        // Navigate to Arithmetic page
-        window.location.href = 'arithmetic.html';
-    });
+    // // Handle navigation to different pages
+    // document.getElementById('arithmetic-btn').addEventListener('click', () => {
+    //     // Navigate to Arithmetic page
+    //     window.location.href = 'arithmetic.html';
+    // });
 
-    document.getElementById('statistics-btn').addEventListener('click', () => {
-        // Navigate to Statistics page
-        window.location.href = 'statistics.html';
-    });
+    // document.getElementById('statistics-btn').addEventListener('click', () => {
+    //     // Navigate to Statistics page
+    //     window.location.href = 'statistics.html';
+    // });
 
     document.getElementById('algebra-btn').addEventListener('click', () => {
         // Navigate to Algebra page
