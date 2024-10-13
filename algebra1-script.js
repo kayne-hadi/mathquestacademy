4f780f34db3048a957846b0cbce2c887b3106df5
kayne-hadi <krhadipoespito@gmail.com>
2024-10-13 21:31:22 +0700

    Update algebra1-script.js

--- algebra1-script.js
+++ algebra1-script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let equation = '';
         
         // Generate a random coefficient between 2 and 20
-        const coefficient = Math.floor(Math.random() * 40) + 1;
+        const coefficient = Math.floor(Math.random() * 30) + 1;
         
         // Generate a constant that is a multiple of the coefficient
         // This guarantees the result will be a whole number
